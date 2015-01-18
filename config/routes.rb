0ae46b19f189c84de2f29f8b8502b43f429d56b2
Matt Hooper <matthewhooper91@gmail.com>
2015-01-18 12:34:18 -0500

    Add users.
    Add user and user session resources.
    Add registering, login, and logout.

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,8 @@
 Rails.application.routes.draw do
+  resources :users, :user_sessions
+  get 'login' => 'user_sessions#new', as: :login
+  get 'logout' => 'user_sessions#destroy', as: :logout
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 
