b6b0e18131b03e7a941c1c35714ccac46f7b0cca
Matt Hooper <matthewhooper91@gmail.com>
2015-01-24 15:51:04 -0500

    Add minimal validation to post and board.

--- app/models/board.rb
+++ app/models/board.rb
@@ -1,4 +1,6 @@
 class Board < ActiveRecord::Base
+	validates :title, presence: true
+
 	has_many :moderators
 	has_many :users, through: :moderators
 
--- app/models/post.rb
+++ app/models/post.rb
@@ -1,4 +1,7 @@
 class Post < ActiveRecord::Base
+	validates :title, presence: true
+	validates :link, presence: true
+	
   belongs_to :user
   belongs_to :board
 end
